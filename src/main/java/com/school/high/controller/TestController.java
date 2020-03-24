110288f373f6be23eb833298f576a9d641988804
BHUVANESWARI <BHUVANESWARI@192.168.1.5>
2020-03-24 12:38:30 +0530

    CEM102 Adding post method and swagger to the main branch

--- src/main/java/com/school/high/controller/TestController.java
+++ src/main/java/com/school/high/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.school.high.controller;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +36,16 @@ public class TestController {
 		
 		
 	}
+	
+	@PostMapping("/setNewSchool")
+	ResponseSchoolDetails setNewSchool(@RequestBody ResponseSchoolDetails responseSchoolDetails)
+	{
+		if(!StringUtils.isEmpty(responseSchoolDetails.getSchoolName()))
+		return responseSchoolDetails;
+		else
+		return null;
+	}
+	
+	
 
 }
