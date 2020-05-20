6142e1a381724992bb33f1f97b6167e911e2452d
sivakumar1987 <7867793+sivakumar1987@users.noreply.github.com>
2020-05-20 20:57:59 +0530

    Removing config from application.properties

--- src/main/java/com/school/high/jwtconfigure/JwtTokenUtil.java
+++ src/main/java/com/school/high/jwtconfigure/JwtTokenUtil.java
@@ -21,8 +21,8 @@ public class JwtTokenUtil implements Serializable {
 
 	public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
-	@Value("${jwt.secret}")
-	private String secret;
+	
+	private String secret="schoolsecret";
 
 	//retrieve username from jwt token
 	public String getUsernameFromToken(String token) {
